4227877cbe391e1d8635396901764b87d12248f7
averbusem <semyontechguru@gmail.com>
2024-10-31 21:47:15 +0300

    увеличил скорость персонажа в ветке анимации

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    private float movement_speed = 300;
+    private float movement_speed = 350;
     private bool isSwordReloading= false;
     private float reloadTime = 2f;
 
