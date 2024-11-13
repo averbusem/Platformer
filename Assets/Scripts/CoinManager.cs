10d0f26f99696ed366ea43df074e6526709a999f
KiraAMonev <kamonev@edu.hse.ru>
2024-11-13 23:27:44 +0300

    CoinIMGs were replaced on CoinPrefab. Also Player can stack all coins and lost part of all coins when he dies

--- Assets/Scripts/CoinManager.cs
+++ Assets/Scripts/CoinManager.cs
@@ -1,28 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class CoinManager : MonoBehaviour
 {
+    public static CoinManager instance;
 
-    public Text coinText;
-    private  int coinCount = 0;
+    private Text coinText;
+    private int totalCoins = 0;
+    private int currentCoin = 0;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     void Start()
     {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        UpdateUI();
         UpdateCoinText();
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        UpdateUI();
+        UpdateCoinText();
+    }
+
+    private void UpdateUI()
+    {
+        GameObject coinTextObject = GameObject.Find("PlayerCoinsCount");
+        if (coinTextObject != null)
+        {
+            coinText = coinTextObject.GetComponent<Text>();
+        }
     }
 
     public void AddCoin()
     {
-        coinCount++;
+        currentCoin++;
+        totalCoins++;
         UpdateCoinText();
     }
 
     private void UpdateCoinText()
     {
-        coinText.text = coinCount.ToString();
+        if (coinText != null)
+        {
+            coinText.text = totalCoins.ToString();
+        }
     }
 
+    public void ResetLevelCoins()
+    {
+        totalCoins -= currentCoin;
+        currentCoin = 0;
+        UpdateCoinText();
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
 }
